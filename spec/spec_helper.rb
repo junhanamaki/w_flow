7276b2d1b6bca2a683d3e6b1af2a0550094c233d
junhanamaki <jun.hanamaki@gmail.com>
2015-05-09 20:15:03 +0100

    movindg aroudn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,13 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+#$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+if ENV['CODECLIMATE_REPO_TOKEN'].nil?
+  require 'simplecov'
+  SimpleCov.start do
+    coverage_dir 'tmp/coverage'
+  end
+else
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
+
 require 'w_flow'
