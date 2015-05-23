ca76aa252ccd62aeddbaefd543cde8dd52475f19
junhanamaki <jun.hanamaki@gmail.com>
2015-05-23 15:12:55 +0100

    data testing

--- spec/w_flow/process/data_helper_spec.rb
+++ spec/w_flow/process/data_helper_spec.rb
@@ -15,13 +15,13 @@ describe 'class that includes WFlow::Process' do
       end
     end
   end
-  before         { @report = mock_class.run(reader: 'reader_val', accessor: 'accessor_val') }
+  before         { @report = mock_class.run(reader: 'reader_val') }
   let(:instance) { @report.data.instance }
   let(:flow)     { @report.data.flow }
 
   describe '.data_reader' do
     context "when invoked" do
-      it 'creates methods with passed attribute names' do
+      it 'creates getter methods with passed attribute names' do
         expect(instance).to respond_to(:reader)
       end
 
@@ -34,9 +34,9 @@ describe 'class that includes WFlow::Process' do
   describe '.data_writer' do
     context 'when invoked' do
       let(:writer_val) { 'writer_val' }
-      before { instance.writer = writer_val }
+      before           { instance.writer = writer_val }
 
-      it 'creates methods with passed attribute names' do
+      it 'creates setter methods with passed attribute names' do
         expect(instance).to respond_to('writer=')
       end
 
@@ -47,5 +47,25 @@ describe 'class that includes WFlow::Process' do
   end
 
   describe '.data_accessor' do
+    context 'when invoked' do
+      let(:accessor_val) { 'new_accessor_val' }
+      before { instance.accessor = accessor_val }
+
+      it 'creates getter methods with passed attribute names' do
+        expect(instance).to respond_to(:accessor)
+      end
+
+      it 'creates setter methods with passed attribute names' do
+        expect(instance).to respond_to("accessor=")
+      end
+
+      it 'created getter method returns value in flow.data.<attribute name>' do
+        expect(instance.accessor).to eq(flow.data.accessor)
+      end
+
+      it 'created setter method sets value in flow.data.<attribute name>' do
+        expect(flow.data.accessor).to eq(accessor_val)
+      end
+    end
   end
 end
