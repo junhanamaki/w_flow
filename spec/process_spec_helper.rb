85fd634234d7d28fdabd0e8d4685c919b456fcc0
junhanamaki <jun.hanamaki@gmail.com>
2015-05-24 19:16:26 +0100

    looking good

--- spec/process_spec_helper.rb
+++ spec/process_spec_helper.rb
@@ -1,24 +1,27 @@
 class Base
   include WFlow::Process
 
-  attr_reader :execution_order
+  data_reader :execution_order
 end
 
 class AProcess < Base
-  def setup;   execution_order << 'a_setup'; end
-  def perform; execution_order << 'a_perform'; end
-  def final;   execution_order << 'a_final'; end
+  def setup;    execution_order << 'a_setup';    end
+  def perform;  execution_order << 'a_perform';  end
+  def finalize; execution_order << 'a_finalize'; end
+  def rollback; execution_order << 'a_rollback'; end
 end
 
 class BProcess < Base
-  def setup;   execution_order << 'b_setup';   end
-  def perform; execution_order << 'b_perform'; end
-  def final;   execution_order << 'b_final';   end
+  def setup;    execution_order << 'b_setup';    end
+  def perform;  execution_order << 'b_perform';  end
+  def finalize; execution_order << 'b_finalize'; end
+  def rollback; execution_order << 'b_rollback'; end
 end
 
 class CProcess < Base
-  def setup;   execution_order << 'c_setup'; end
+  def setup;    execution_order << 'c_setup';    end
   execute AProcess
-  def perform; execution_order << 'c_perform'; end
-  def final;   execution_order << 'c_final'; end
+  def perform;  execution_order << 'c_perform';  end
+  def finalize; execution_order << 'c_finalize'; end
+  def rollback; execution_order << 'c_rollback'; end
 end
--- spec/w_flow/process/execution_order_spec.rb
+++ spec/w_flow/process/execution_order_spec.rb
@@ -2,8 +2,28 @@ require 'spec_helper'
 require 'process_spec_helper'
 
 describe 'class that includes WFlow::Process' do
+  let(:params) { { execution_order: [] } }
+
   describe '.run' do
-    it '' do
+    context 'when invoked on AProcess' do
+      before { @report = AProcess.run(params) }
+      let(:execution_order) { @report.data.execution_order }
+
+      it 'returned execution_order has only 3 entries' do
+        expect(execution_order.count).to eq(3)
+      end
+
+      it 'executes setup first' do
+        expect(execution_order[0]).to eq('a_setup')
+      end
+
+      it 'executes perform second' do
+        expect(execution_order[1]).to eq('a_perform')
+      end
+
+      it 'executes finalize last' do
+        expect(execution_order[2]).to eq('a_finalize')
+      end
     end
   end
 end
